3a4ea42fad6de5895c379a3dae0fd3dac5452b09
cpm5041 <christopher.marcenelle@libertymutual.com>
2017-04-13 16:08:07 -0400

    Switching config files

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const config = {
-  apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
-  }
-}
 // const config = {
 //   apiOrigins: {
-//     development: 'https://localhost:4741',
-//     production: 'https://aqueous-atoll-85096.herokuapp.com/'
+//     production: 'https://ga-wdi-boston.herokuapp.com'
 //   }
 // }
+const config = {
+  apiOrigins: {
+    development: 'https://localhost:4741',
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
+  }
+}
 
 module.exports = config
